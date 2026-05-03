6f2402971628e61c685e597321408113ce4845df
Chiman2937 <cyga2933@gmail.com>
2026-05-04 01:31:02 +0900

    feat: Icon 목록 추가

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,12 +1,15 @@
 import IconCalendarActive from '@/assets/icons/icon-calendar-1-active.svg';
 import IconCalendarDefault from '@/assets/icons/icon-calendar-1-default.svg';
+import IconCalendarFour from '@/assets/icons/icon-calendar-4.svg';
 import IconHomeActive from '@/assets/icons/icon-home-active.svg';
 import IconHomeDefault from '@/assets/icons/icon-home-default.svg';
+import IconMapPinFive from '@/assets/icons/icon-map-pin-5.svg';
 import IconMessageActive from '@/assets/icons/icon-message-active.svg';
 import IconMessageDefault from '@/assets/icons/icon-message-default.svg';
 import IconPost from '@/assets/icons/icon-plus-circle.svg';
 import IconUserOneActive from '@/assets/icons/icon-user-1-active.svg';
 import IconUserOneDefault from '@/assets/icons/icon-user-1-default.svg';
+import IconUserTwo from '@/assets/icons/icon-users-2.svg';
 
 const ICONS_MAP = {
   'home-default': IconHomeDefault,
@@ -18,6 +21,9 @@ const ICONS_MAP = {
   'message-active': IconMessageActive,
   'user-1-default': IconUserOneDefault,
   'user-1-active': IconUserOneActive,
+  'user-2': IconUserTwo,
+  'calendar-4': IconCalendarFour,
+  'map-pin-5': IconMapPinFive,
 };
 
 export type IconType = keyof typeof ICONS_MAP;
