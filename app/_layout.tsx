645028274803a7c8dbe830f64a325c84bb16fbc6
Chiman2937 <cyga2933@gmail.com>
2026-05-04 12:26:16 +0900

    feat: dayjs 타임존 설정 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,11 +7,16 @@ import {
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 import { Stack } from 'expo-router';
 import { useColorScheme } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 dayjs.extend(relativeTime);
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('Asia/Seoul');
 dayjs.locale('ko');
 
 export default function RootLayout() {
