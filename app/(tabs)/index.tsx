1e063a266bcbdfff171351939679ba0003f03397
Chiman2937 <cyga2933@gmail.com>
2026-05-04 16:05:31 +0900

    feat: group 목록 empty 화면 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
+import ImageEmpty from '@/assets/images/image-empty-1.png';
 import { ListCard } from '@/components/groups/ListCard';
 import { SearchInput } from '@/components/groups/SearchInput';
 import { PageLayout } from '@/components/PageLayout';
 import { Text } from '@/components/Text';
 import { mockGroups } from '@/mock/groups';
+import { Image } from 'expo-image';
 import { useLocalSearchParams } from 'expo-router';
 import { FlatList, View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
-
 export default function Tab() {
   const { search: searchParam } = useLocalSearchParams<{ search: string }>();
 
@@ -33,6 +34,14 @@ export default function Tab() {
         renderItem={({ item }) => <ListCard group={item} />}
         keyExtractor={(item) => item.id.toString()}
       />
+      {nextGroups.length === 0 && (
+        <View style={styles.emptyContainer}>
+          <Image source={ImageEmpty} style={{ width: 140, height: 140 }} />
+          <Text variant="text-sm-medium" style={styles.emptyText}>
+            검색 결과가 없어요.
+          </Text>
+        </View>
+      )}
     </PageLayout>
   );
 }
@@ -60,5 +69,15 @@ const styles = StyleSheet.create((theme) => ({
   ListContent: {
     gap: 16,
     paddingBottom: 16,
+  },
+  emptyContainer: {
+    position: 'absolute',
+    inset: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 8,
+  },
+  emptyText: {
+    color: theme.colors['gray-600'],
   },
 }));
