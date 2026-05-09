0f6a9ed562e270564376a37f1a3f770a2182681d
Chiman2937 <cyga2933@gmail.com>
2026-05-09 14:37:41 +0900

    feat: 로그인 페이지 제작

---
+++ app/login.tsx
@@ -0,0 +1,144 @@
+import { Footer } from '@/components/auth/Footer';
+import { PasswordInput } from '@/components/auth/PasswordInput';
+import { Hint } from '@/components/fields/Hint';
+import { Input } from '@/components/fields/Input';
+import { Label } from '@/components/fields/Label';
+import { Logo } from '@/components/Logo';
+import { PageLayout } from '@/components/PageLayout';
+import { Text } from '@/components/Text';
+import { useForm } from '@tanstack/react-form';
+import { useRouter } from 'expo-router';
+import { Pressable, ScrollView, View } from 'react-native';
+import { StyleSheet } from 'react-native-unistyles';
+import { z } from 'zod';
+
+export default function Login() {
+  const router = useRouter();
+  const schema = z.object({
+    email: z.email('올바른 이메일을 입력해주세요'),
+    password: z.string().min(8, '8자 이상 입력해주세요'),
+  });
+
+  const form = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: async ({ value }) => {
+      // Do something with form data
+      console.log(value);
+    },
+  });
+
+  return (
+    <PageLayout>
+      <ScrollView style={styles.container}>
+        <View style={styles.logo}>
+          <Logo variant="lg" />
+        </View>
+        <View style={styles.fields}>
+          <form.Field
+            name="email"
+            validators={{ onChange: schema.shape.email }}
+          >
+            {(field) => (
+              <View>
+                <Label required>이메일</Label>
+                <Input
+                  field={field}
+                  placeholder="이메일을 입력해주세요"
+                  onButtonPress={() => {}}
+                />
+                <Hint field={field} />
+              </View>
+            )}
+          </form.Field>
+
+          <form.Field
+            name="password"
+            validators={{ onChange: schema.shape.password }}
+          >
+            {(field) => (
+              <View>
+                <Label required>비밀번호</Label>
+                <PasswordInput
+                  field={field}
+                  placeholder="비밀번호를 입력해주세요"
+                />
+                <Hint field={field} />
+              </View>
+            )}
+          </form.Field>
+        </View>
+        <View style={styles.buttonContainer}>
+          <form.Subscribe
+            selector={(state) => [state.canSubmit, state.isSubmitting]}
+            children={([canSubmit, isSubmitting]) => (
+              <Pressable
+                onPress={form.handleSubmit}
+                style={styles.submitButton}
+                disabled={!canSubmit}
+              >
+                <Text variant="text-md-bold" style={styles.submitText}>
+                  {!isSubmitting ? '로그인하기' : '...'}
+                </Text>
+              </Pressable>
+            )}
+          />
+        </View>
+        <View style={styles.footerContainer}>
+          <Footer
+            title="WEGO가 처음이신가요?"
+            rightButtonText="회원가입하기"
+            onPress={() => router.replace('/signup')}
+          />
+        </View>
+      </ScrollView>
+    </PageLayout>
+  );
+}
+
+const styles = StyleSheet.create((theme) => ({
+  container: {
+    padding: 16,
+  },
+  logo: {
+    alignItems: 'center',
+    paddingBottom: 16,
+  },
+  fields: {
+    gap: 16,
+    paddingBottom: 32,
+  },
+  term: {
+    flexDirection: 'row',
+  },
+  termField: {
+    paddingBottom: 16,
+    flex: 1,
+  },
+  termText: {
+    color: theme.colors['gray-700'],
+    flex: 1,
+  },
+  termShowButtonText: {
+    color: theme.colors['gray-500'],
+    textDecorationLine: 'underline',
+  },
+  buttonContainer: {
+    paddingBottom: 40,
+  },
+  submitButton: {
+    height: 52,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: theme.colors['gray-400'],
+    borderRadius: 16,
+  },
+  submitText: {
+    color: theme.colors['mono-white'],
+  },
+  footerContainer: {
+    alignItems: 'center',
+  },
+}));
