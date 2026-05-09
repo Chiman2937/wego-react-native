40e3feb1a398dfc8aaf284bf0c6b67c29a972a96
Chiman2937 <cyga2933@gmail.com>
2026-05-09 13:05:53 +0900

    feat: 회원가입 페이지 필드 배치

---
+++ app/signup.tsx
@@ -0,0 +1,127 @@
+import { DuplicateCheckInput } from '@/components/auth/DuplicateCheckInput';
+import { PasswordInput } from '@/components/auth/PasswordInput';
+import { Hint } from '@/components/fields/Hint';
+import { Label } from '@/components/fields/Label';
+import { PageLayout } from '@/components/PageLayout';
+import { Text } from '@/components/Text';
+import { useForm } from '@tanstack/react-form';
+import { Pressable, ScrollView, View } from 'react-native';
+import { StyleSheet } from 'react-native-unistyles';
+import { z } from 'zod';
+
+export default function Signup() {
+  const schema = z.object({
+    email: z.email('올바른 이메일을 입력해주세요'),
+    nickname: z.string().min(1, '닉네임을 입력해주세요'),
+    password: z.string().min(8, '8자 이상 입력해주세요'),
+    passwordConfirm: z.string().min(8, '8자 이상 입력해주세요'),
+  });
+
+  const form = useForm({
+    defaultValues: {
+      email: '',
+      nickname: '',
+      password: '',
+      passwordConfirm: '',
+    },
+    onSubmit: async ({ value }) => {
+      // Do something with form data
+      console.log(value);
+    },
+  });
+
+  return (
+    <PageLayout>
+      <ScrollView style={styles.container}>
+        <View style={styles.fields}>
+          <form.Field
+            name="email"
+            validators={{ onChange: schema.shape.email }}
+          >
+            {(field) => (
+              <View>
+                <Label isRequired>이메일</Label>
+                <DuplicateCheckInput
+                  field={field}
+                  placeholder="이메일을 입력해주세요"
+                  onButtonPress={() => {}}
+                />
+                <Hint field={field} />
+              </View>
+            )}
+          </form.Field>
+          <form.Field
+            name="nickname"
+            validators={{ onChange: schema.shape.nickname }}
+          >
+            {(field) => (
+              <View>
+                <Label isRequired>닉네임</Label>
+                <DuplicateCheckInput
+                  field={field}
+                  placeholder="닉네임을 입력해주세요"
+                  onButtonPress={() => {}}
+                />
+                <Hint field={field} />
+              </View>
+            )}
+          </form.Field>
+          <form.Field
+            name="password"
+            validators={{ onChange: schema.shape.password }}
+          >
+            {(field) => (
+              <View>
+                <Label isRequired>비밀번호</Label>
+                <PasswordInput
+                  field={field}
+                  placeholder="비밀번호를 입력해주세요"
+                />
+                <Hint field={field} />
+              </View>
+            )}
+          </form.Field>
+          <form.Field
+            name="passwordConfirm"
+            validators={{ onChange: schema.shape.passwordConfirm }}
+          >
+            {(field) => (
+              <View>
+                <Label isRequired>비밀번호 확인</Label>
+                <PasswordInput
+                  field={field}
+                  placeholder="비밀번호를 한번 더 입력해주세요"
+                />
+                <Hint field={field} />
+              </View>
+            )}
+          </form.Field>
+        </View>
+        <Pressable onPress={form.handleSubmit} style={styles.submitButton}>
+          <Text variant="text-md-bold" style={styles.submitText}>
+            회원가입 하기
+          </Text>
+        </Pressable>
+      </ScrollView>
+    </PageLayout>
+  );
+}
+
+const styles = StyleSheet.create((theme) => ({
+  container: {
+    padding: 16,
+  },
+  fields: {
+    gap: 16,
+  },
+  submitButton: {
+    height: 52,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: theme.colors['gray-400'],
+    borderRadius: 16,
+  },
+  submitText: {
+    color: theme.colors['mono-white'],
+  },
+}));
