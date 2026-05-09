fe521fb8aa183303de02eedf95db142fbbdae958
Chiman2937 <cyga2933@gmail.com>
2026-05-09 12:33:51 +0900

    fix: Header에 로그인 버튼 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,21 @@
 import { View } from 'react-native';
 
-import IconBell from '@/assets/icons/icon-bell.svg';
+// import IconBell from '@/assets/icons/icon-bell.svg';
 import IconLogo from '@/assets/icons/icon-logo.svg';
+import { Link } from 'expo-router';
 import { StyleSheet } from 'react-native-unistyles';
+import { Text } from './Text';
 
 export const Header = () => {
   return (
     <View accessibilityRole="header" style={styles.container}>
       <IconLogo />
-      <IconBell />
+      {/* <IconBell /> */}
+      <Link href={'/signup'}>
+        <Text variant="text-sm-semibold" style={styles.text}>
+          로그인
+        </Text>
+      </Link>
     </View>
   );
 };
@@ -19,8 +26,13 @@ const styles = StyleSheet.create((theme) => ({
     height: 56,
     flexDirection: 'row',
     alignItems: 'center',
-    paddingHorizontal: 8,
-    paddingVertical: 16,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
     justifyContent: 'space-between',
+  },
+  text: {
+    color: theme.colors['gray-500'],
+    paddingVertical: 4,
+    paddingHorizontal: 8,
   },
 }));
