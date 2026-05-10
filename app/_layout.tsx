d0772b30b579c4b7a31ea4882ef8c78e5eb22ff3
Chiman2937 <cyga2933@gmail.com>
2026-05-10 11:01:22 +0900

    feat: RootLayout에 GestureHandlerRootView 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,6 +11,7 @@ import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
 import { Stack } from 'expo-router';
 import { useColorScheme } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
@@ -23,19 +24,21 @@ dayjs.locale('ko');
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
-    <SafeAreaProvider>
-      <KeyboardProvider>
-        <ThemeProvider
-          value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
-        >
-          <SafeAreaView style={{ flex: 1 }} edges={['top']}>
-            <Header />
-            <Stack screenOptions={{ headerShown: false }}>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            </Stack>
-          </SafeAreaView>
-        </ThemeProvider>
-      </KeyboardProvider>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <KeyboardProvider>
+          <ThemeProvider
+            value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+          >
+            <SafeAreaView style={{ flex: 1 }} edges={['top']}>
+              <Header />
+              <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              </Stack>
+            </SafeAreaView>
+          </ThemeProvider>
+        </KeyboardProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
